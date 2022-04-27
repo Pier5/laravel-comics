a9df5704371e2870285b9216cf66cb4179a2ac50
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-04-27 18:49:00 +0200

    -footer-

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -2,8 +2,8 @@
     <div class="header-top">
         <div class="container-head_top container">
             <ul>
-                <li>dc power visa</li>
-                <li>addictional dc sites<i class="fa-solid fa-angle-down"></i></li>
+                <li class="header-li">dc power™ visa®</li>
+                <li class="header-li">addictional dc sites<i class="fa-solid fa-angle-down"></i></li>
             </ul>
         </div>
     </div>
@@ -12,16 +12,16 @@
             <img class="header-logo" src="./images/dc-logo.png" alt="logo dc">
             <nav>
                 <ul>
-                    <a href=""><li class="li-head_bottom">Characters</li></a>
-                    <a href=""><li class="li-head_bottom">Comics</li></a>
-                    <a href=""><li class="li-head_bottom">Movies</li></a>
-                    <a href=""><li class="li-head_bottom">Tv</li></a>
-                    <a href=""><li class="li-head_bottom">Games</li></a>
-                    <a href=""><li class="li-head_bottom">Collectibles</li></a>
-                    <a href=""><li class="li-head_bottom">Videos</li></a>
-                    <a href=""><li class="li-head_bottom">Fans</li></a>
-                    <a href=""><li class="li-head_bottom">News</li></a>
-                    <a href=""><li class="li-head_bottom">Shop</li><i class="fa-solid fa-angle-down"></i></a>
+                    <a href=""><li class="header-li li-head_bottom">Characters</li></a>
+                    <a href=""><li class="header-li li-head_bottom">Comics</li></a>
+                    <a href=""><li class="header-li li-head_bottom">Movies</li></a>
+                    <a href=""><li class="header-li li-head_bottom">Tv</li></a>
+                    <a href=""><li class="header-li li-head_bottom">Games</li></a>
+                    <a href=""><li class="header-li li-head_bottom">Collectibles</li></a>
+                    <a href=""><li class="header-li li-head_bottom">Videos</li></a>
+                    <a href=""><li class="header-li li-head_bottom">Fans</li></a>
+                    <a href=""><li class="header-li li-head_bottom">News</li></a>
+                    <a href=""><li class="header-li li-head_bottom">Shop</li><i class="fa-solid fa-angle-down"></i></a>
                 </ul>
             </nav>
             <div class="search-container">
