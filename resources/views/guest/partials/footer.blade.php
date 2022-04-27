31d159cd314ca096f7fcdd5f98506eb4ec15c191
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-04-27 17:40:16 +0200

    -footer_bottom-

--- resources/views/guest/partials/footer.blade.php
+++ resources/views/guest/partials/footer.blade.php
@@ -20,9 +20,23 @@
     </div>
 
 
-    <div class="footer-main"></div>
+    <div class="footer-middle">
+
+    </div>
 
 
 
-    <div class="footer-bottom"></div>
+    <div class="footer-bottom">
+        <div class="container container-footer_bottom">
+            <div class="sign"><a class="sign-link" href="">Sign up now!</a></div>
+            <div class="socials">
+                <div class="foot-bottom_text">Follow us</div>
+                <a href=""><img class="social-img" src="./images/footer-facebook.png" alt="logo fb"></a>
+                <a href=""><img class="social-img" src="./images/footer-twitter.png" alt=""></a>
+                <a href=""><img class="social-img" src="./images/footer-youtube.png" alt=""></a>
+                <a href=""><img class="social-img" src="./images/footer-pinterest.png" alt=""></a>
+                <a href=""><img class="social-img" src="./images/footer-periscope.png" alt=""></a>
+            </div>
+        </div>
+    </div>
 </footer>
