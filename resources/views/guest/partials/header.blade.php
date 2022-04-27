504ec565ef7ebd551176cfa9581ea46b2bac352c
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-04-27 16:14:10 +0200

    -header- /il css è approssimativo

---
+++ resources/views/guest/partials/header.blade.php
@@ -0,0 +1,33 @@
+<header>
+    <div class="header-top">
+        <div class="container-head_top container">
+            <ul>
+                <li>dc power visa</li>
+                <li>addictional dc sites<i class="fa-solid fa-angle-down"></i></li>
+            </ul>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="container-head_bottom container">
+            <img class="header-logo" src="./images/dc-logo.png" alt="">
+            <nav>
+                <ul>
+                    <a href=""><li class="li-head_bottom">Characters</li></a>
+                    <a href=""><li class="li-head_bottom">Comics</li></a>
+                    <a href=""><li class="li-head_bottom">Movies</li></a>
+                    <a href=""><li class="li-head_bottom">Tv</li></a>
+                    <a href=""><li class="li-head_bottom">Games</li></a>
+                    <a href=""><li class="li-head_bottom">Collectibles</li></a>
+                    <a href=""><li class="li-head_bottom">Videos</li></a>
+                    <a href=""><li class="li-head_bottom">Fans</li></a>
+                    <a href=""><li class="li-head_bottom">News</li></a>
+                    <a href=""><li class="li-head_bottom">Shop</li><i class="fa-solid fa-angle-down"></i></a>
+                </ul>
+            </nav>
+            <div class="search-container">
+                <input type="text" name="search" id="search" placeholder="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </div>
+    </div>
+</header>
